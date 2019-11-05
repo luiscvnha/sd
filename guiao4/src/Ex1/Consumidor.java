8a10c25f7c0dce1878cba749684fa9a058dd0bf6
Luís Cunha <40152994+luiscvnha@users.noreply.github.com>
2019-11-05 16:53:53 +0000

    Update Consumidor.java

--- guiao4/src/Ex1/Consumidor.java
+++ guiao4/src/Ex1/Consumidor.java
@@ -3,9 +3,7 @@ package Ex1;
 public class Consumidor implements Runnable {
     private BoundedBuffer buf;
 
-    public Consumidor(BoundedBuffer b) {
-        buf = b;
-    }
+    public Consumidor(BoundedBuffer b) { buf = b; }
 
     @Override
     public void run() {
