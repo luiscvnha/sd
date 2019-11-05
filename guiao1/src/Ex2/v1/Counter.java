ff2a3a39351d41e664b98b5cb02f8af26180b327
Luís Cunha <40152994+luiscvnha@users.noreply.github.com>
2019-11-05 17:02:26 +0000

    Update Counter.java

--- guiao1/src/Ex2/v1/Counter.java
+++ guiao1/src/Ex2/v1/Counter.java
@@ -3,9 +3,9 @@ package Ex2.v1;
 public class Counter {
     private int x;
 
-    public Counter() {x = 0;}
+    public Counter() { x = 0; }
 
-    public synchronized void increment() {++x;}
+    public void increment() { ++x; }
 
-    public int get() {return x;}
+    public int get() { return x; }
 }
