2572bd076558badc6030dedd0bfe7faf98932759
Luís Cunha <40152994+luiscvnha@users.noreply.github.com>
2019-11-13 09:23:00 +0000

    Update RWLockFairReader.java

--- guiao5/src/Ex3/RWLockFairReader.java
+++ guiao5/src/Ex3/RWLockFairReader.java
@@ -3,7 +3,7 @@ package Ex3;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class RWLockFairReader implements RWLock {
+public class RWLockFairReader {
     private ReentrantLock lock;
     private Condition readersCond;
     private Condition writersCond;
