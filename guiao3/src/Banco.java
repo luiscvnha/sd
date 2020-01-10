34caa312042cd8efd500ae8a37038b22a965ca72
Luís Cunha <luiscvnha@gmail.com>
2020-01-10 21:42:32 +0000

    Update Banco.java

--- guiao3/src/Banco.java
+++ guiao3/src/Banco.java
@@ -1,62 +1,63 @@
+import java.util.Map;
 import java.util.HashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Banco {
-    private HashMap<Integer,Conta> contas;
+    private Map<Integer, Conta> contas;
     private ReentrantLock lockBanco;
     private int nextId = 0;
 
     public Banco(int n) {
-        this.contas = new HashMap<>();
-        this.lockBanco = new ReentrantLock();
+        contas = new HashMap<>();
+        lockBanco = new ReentrantLock();
         for (int i = 0; i < n; ++i)
             criarConta(0.0);
     }
 
     public int criarConta(double saldoInicial) {
-        this.lockBanco.lock();
+        lockBanco.lock();
 
         int id = nextId++;
         Conta conta = new Conta(id, saldoInicial);
-        this.contas.put(id, conta);
+        contas.put(id, conta);
 
-        this.lockBanco.unlock();
+        lockBanco.unlock();
 
         return id;
     }
 
     public double fecharConta(int id) throws ContaInvalida {
-        this.lockBanco.lock();
+        lockBanco.lock();
 
-        if (!this.contas.containsKey(id)) {
-            this.lockBanco.unlock();
+        if (!contas.containsKey(id)) {
+            lockBanco.unlock();
             throw new ContaInvalida(id);
         }
 
-        Conta conta = this.contas.get(id);
+        Conta conta = contas.get(id);
         conta.lock();
 
         double saldo = conta.consultar();
-        this.contas.remove(id);
+        contas.remove(id);
 
-        this.lockBanco.unlock();
+        lockBanco.unlock();
         conta.unlock();
 
         return saldo;
     }
 
     public double consultar(int id) throws ContaInvalida {
-        this.lockBanco.lock();
+        lockBanco.lock();
 
-        if (!this.contas.containsKey(id)) {
-            this.lockBanco.unlock();
+        if (!contas.containsKey(id)) {
+            lockBanco.unlock();
             throw new ContaInvalida(id);
         }
 
-        Conta conta = this.contas.get(id);
+        Conta conta = contas.get(id);
         conta.lock();
 
-        this.lockBanco.unlock();
+        lockBanco.unlock();
 
         double r = conta.consultar();
         conta.unlock();
@@ -68,14 +69,14 @@ public class Banco {
         Conta[] contas = new Conta[id_contas.length];
         int len = 0;
 
-        this.lockBanco.lock();
+        lockBanco.lock();
         for (int id: id_contas)
             if (this.contas.containsKey(id)) {
                 contas[len] = this.contas.get(id);
                 contas[len].lock();
                 ++len;
             }
-        this.lockBanco.unlock();
+        lockBanco.unlock();
 
         double r = 0.0;
 
@@ -88,19 +89,19 @@ public class Banco {
     }
 
     public void levantar(int id, double valor) throws ContaInvalida, SaldoInsuficiente {
-        this.lockBanco.lock();
+        lockBanco.lock();
 
-        if (!this.contas.containsKey(id)) {
+        if (!contas.containsKey(id)) {
             // conta não existe
-            this.lockBanco.unlock();
+            lockBanco.unlock();
             throw new ContaInvalida(id);
         }
 
         // caso exista, adquirir lock
-        Conta conta = this.contas.get(id);
+        Conta conta = contas.get(id);
         conta.lock();
 
-        this.lockBanco.unlock();
+        lockBanco.unlock();
 
         // consultar valor
         if (conta.consultar() < valor) {
@@ -114,19 +115,19 @@ public class Banco {
     }
 
     public void depositar(int id, double valor) throws ContaInvalida {
-        this.lockBanco.lock();
+        lockBanco.lock();
 
-        if (!this.contas.containsKey(id)) {
+        if (!contas.containsKey(id)) {
             // conta não existe
-            this.lockBanco.unlock();
+            lockBanco.unlock();
             throw new ContaInvalida(id);
         }
 
         // caso exista, adquirir lock
-        Conta conta = this.contas.get(id);
+        Conta conta = contas.get(id);
         conta.lock();
 
-        this.lockBanco.unlock();
+        lockBanco.unlock();
 
         // depositar valor
         conta.depositar(valor);
@@ -135,22 +136,22 @@ public class Banco {
     }
 
     public void transferir(int id_origem, int id_destino, double valor) throws ContaInvalida, SaldoInsuficiente {
-        this.lockBanco.lock();
+        lockBanco.lock();
 
-        if (!this.contas.containsKey(id_origem)) {
-            this.lockBanco.unlock();
+        if (!contas.containsKey(id_origem)) {
+            lockBanco.unlock();
             throw new ContaInvalida(id_origem);
-        } else if (!this.contas.containsKey(id_destino)) {
-            this.lockBanco.unlock();
+        } else if (!contas.containsKey(id_destino)) {
+            lockBanco.unlock();
             throw new ContaInvalida(id_destino);
         }
 
-        Conta c_origem = this.contas.get(id_origem);
+        Conta c_origem = contas.get(id_origem);
         c_origem.lock();
-        Conta c_destino = this.contas.get(id_destino);
+        Conta c_destino = contas.get(id_destino);
         c_destino.lock();
 
-        this.lockBanco.unlock();
+        lockBanco.unlock();
 
         if (c_origem.consultar() < valor) {
             c_origem.unlock();
